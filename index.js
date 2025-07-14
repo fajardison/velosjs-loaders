d85e884c35859ebf0b8fe11dce63a097628ab04b
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-15 00:25:12 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import fileLoader from './loaders/fileLoader.js'
+import recursiveModuleLoader from './loaders/recursiveModuleLoader.js'
+import patternFileLoader from './loaders/patternFileLoader.js'
+import pluginsLoader from './loaders/pluginsLoader.js'
+
+export {
+  fileLoader,
+  recursiveModuleLoader,
+  patternFileLoader,
+  pluginsLoader
+}
+
+
+export default {
+  fileLoader,
+  recursiveModuleLoader,
+  patternFileLoader,
+  pluginsLoader
+}
