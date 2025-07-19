d65e443727a2711ac13861d32e8f29b3e0497d31
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-07-19 20:42:34 +0700

    Create color.plugin.js

---
+++ sample/plugins/color.plugin.js
@@ -0,0 +1,15 @@
+export default async function Color(app) {
+  const color = {
+    red: (text) => `\x1b[31m${text}\x1b[0m`,
+    green: (text) => `\x1b[32m${text}\x1b[0m`,
+    yellow: (text) => `\x1b[33m${text}\x1b[0m`,
+    blue: (text) => `\x1b[34m${text}\x1b[0m`,
+    magenta: (text) => `\x1b[35m${text}\x1b[0m`,
+    cyan: (text) => `\x1b[36m${text}\x1b[0m`,
+    bold: (text) => `\x1b[1m${text}\x1b[0m`
+  }
+
+  console.log(color.green('[PLUGIN] Colo aktif'))
+  app.color = color
+  return 'plugin-colo'
+}
